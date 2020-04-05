16a580ee3eff0ac564a008bee7265bcc97a53502
Zach Leatherman <zachleatherman@gmail.com>
2020-04-05 14:09:41 -0500

    Adds .log to ignores, reduce test sample

--- test/sample.js
+++ test/sample.js
@@ -3,17 +3,17 @@ const PerfLeaderboard = require("../.");
 (async function() {
 	let urls = [
 		"https://www.gatsbyjs.org/",
-		"https://nextjs.org/",
 		"https://www.11ty.dev/",
-		"https://vuejs.org/",
-		"https://reactjs.org/",
-		"https://amp.dev/",
-		"https://jekyllrb.com/",
-		"https://nuxtjs.org/",
-		"https://gridsome.org/",
-		"https://svelte.dev/",
-		"https://gohugo.io/",
-		"https://redwoodjs.com/"
+		// "https://vuejs.org/",
+		// "https://reactjs.org/",
+		// "https://nextjs.org/",
+		// "https://amp.dev/",
+		// "https://jekyllrb.com/",
+		// "https://nuxtjs.org/",
+		// "https://gridsome.org/",
+		// "https://svelte.dev/",
+		// "https://gohugo.io/",
+		// "https://redwoodjs.com/"
 	];
 
 	console.log( await PerfLeaderboard(urls, 1) );
