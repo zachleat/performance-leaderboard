ae8decd4f23acb48d9bf4b40a1b4fcb7d744f0da
Zach Leatherman <zachleatherman@gmail.com>
2020-11-16 09:45:44 -0600

    Read logs from yesterday if todays log entry does not exist 😅

--- src/ReadLog.js
+++ src/ReadLog.js
@@ -1,15 +1,26 @@
 const path = require("path");
 const fs = require("fs");
 
-function readLog(fileSlug, logDirectory) {
-  let date = new Date().toISOString().substr(0, 10);
+function readLogFromDate(dateObj, fileSlug, logDirectory) {
+  let date = dateObj.toISOString().substr(0, 10);
   let dir = path.join(path.resolve("."), logDirectory, date);
-
+  
   let filepath = path.join(dir, `${fileSlug}`);
   if(fs.existsSync(filepath)) {
     return require(filepath);
   }
   return false;
+}
+
+function readLog(fileSlug, logDirectory) {
+  let result = readLogFromDate(new Date(), fileSlug, logDirectory);
+  if(result !== false) {
+    return result;
+  }
+
+  let yesterday = new Date();
+  yesterday.setTime(yesterday.getTime() - 1000*60*60*23);
+  return readLogFromDate(yesterday, fileSlug, logDirectory);
 }
 
 module.exports = readLog;
