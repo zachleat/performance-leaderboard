70ae3736b50f5894d5d3d27b358050d1a116ef60
Zach Leatherman <zachleatherman@gmail.com>
2021-08-08 21:26:17 -0500

    Update dependencies, removes fs-extra.

--- src/WriteLog.js
+++ src/WriteLog.js
@@ -1,13 +1,16 @@
-const fs = require("fs-extra");
+const fs = require("fs");
+const fsp = fs.promises;
 const path = require("path");
 
 async function writeLog(fileSlug, rawResult, logDirectory) {
   let date = new Date().toISOString().substr(0, 10);
   let dir = path.join(path.resolve("."), logDirectory, date);
-  await fs.ensureDir(dir);
+  await fsp.mkdir(dir, {
+    recursive: true
+  });
 
   let filepath = path.join(dir, `${fileSlug}`);
-  return fs.writeJson(filepath, rawResult, { spaces: 2 });
+  return fsp.writeFile(filepath, JSON.stringify(rawResult, null, 2));
 }
 
 module.exports = writeLog;
